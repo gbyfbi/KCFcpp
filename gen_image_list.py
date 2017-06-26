8a69eaa066dd474ba5eecdfd3c16bdb1678b193d
Boyang Gao <gaoboyang@gmail.com>
2017-06-26 18:59:47 +0200

    add image list generator script.

---
+++ gen_image_list.py
@@ -0,0 +1,10 @@
+from __future__ import print_function
+import glob
+
+image_dir = '/home/gao/ClionProjects/ros_camera_publisher/src/depth_rgb_subscriber/build'
+image_list = glob.glob(image_dir+'/'+'*.jpg')
+output_image_list_path = image_dir + '/' + 'images.txt'
+with open(output_image_list_path, 'w') as f:
+    sorted_image_list = sorted(image_list)
+    for path in sorted_image_list:
+        print(path, file=f)
