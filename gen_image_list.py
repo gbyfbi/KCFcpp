29d4ac1d34bdad2942d1b9276493976f40e2c43d
Boyang Gao <gaoboyang@gmail.com>
2018-01-18 13:47:23 +0100

    before give up Alibaba.

--- gen_image_list.py
+++ gen_image_list.py
@@ -2,7 +2,7 @@ from __future__ import print_function
 import glob
 
 # image_dir = '/home/gao/ClionProjects/ros_camera_publisher/src/depth_rgb_subscriber/build'
-image_dir = '/home/gao/Desktop/image_saved_liu'
+image_dir = '/home/gao/Desktop/image_saved_kuka'
 image_list = glob.glob(image_dir+'/'+'*.jpg')
 output_image_list_path = image_dir + '/' + 'images.txt'
 with open(output_image_list_path, 'w') as f:
